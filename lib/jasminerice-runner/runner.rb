dfb5ef3f89c91f8394838eed7f4eb25fea395229
Peter Kinmond <peterkinmond@gmail.com>
2012-12-30 20:12:40 -0800

    Remove command not supported by Capybara 2

--- lib/jasminerice-runner/runner.rb
+++ lib/jasminerice-runner/runner.rb
@@ -1,17 +1,17 @@
 module Jasminerice
   class Runner
-    
+
     include Capybara::DSL
-    
+
     def capybara_driver
       self.class.capybara_driver || :selenium
     end
-    
+
     def run
       Capybara.default_driver = capybara_driver
       visit "/jasmine"
       puts "Running jasmine specs"
-      wait_until { page.evaluate_script("window.jasmineRiceReporter.finished")}
+      wait_for_results
       passed = page.evaluate_script("window.jasmineRiceReporter.results.passedCount")
       failed = page.evaluate_script("window.jasmineRiceReporter.results.failedCount")
       total = page.evaluate_script("window.jasmineRiceReporter.results.totalCount")
@@ -30,13 +30,23 @@ module Jasminerice
           puts "\n"
         end
       end
-  
+
       if page.evaluate_script("window.jasmineRiceReporter.results.failedCount") == 0
         puts "Jasmine specs passed, yay!"
       else
         raise "Jasmine specs failed"
       end
     end
-    
+
+    def wait_for_results
+      start = Time.now
+      while true
+        break if page.evaluate_script("window.jasmineRiceReporter.finished")
+        if Time.now > start + 5.seconds
+          puts "Results not ready yet"
+        end
+        sleep 1
+      end
+    end
   end
 end
