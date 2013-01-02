db4e6f25ef3b88e96709cbf0e5c02cc06b1276e0
Michael Jensen and Peter Kinmond <engineers+mjensen+peter@sharethrough.com>
2013-01-02 14:38:39 -0800

    Add support for executing multiple jasmine environments

--- lib/jasminerice-runner/runner.rb
+++ lib/jasminerice-runner/runner.rb
@@ -7,16 +7,34 @@ module Jasminerice
       self.class.capybara_driver || :selenium
     end
 
-    def run
+    def run(jasmine_environment)
       Capybara.default_driver = capybara_driver
-      visit "/jasmine"
+      url = "/jasmine"
+
+      if jasmine_environment.present?
+        url += "?environment=#{jasmine_environment}"
+      end
+      visit url
       print "Running jasmine specs"
-      wait_for_finished
+
+      begin
+        wait_for_finished
+      rescue
+        if jasmine_environment.present?
+          filename = "#{jasmine_environment}_spec.js"
+        else
+          filename = "spec.js"
+        end
+        puts "\njasmineRiceReporter not defined! Check your configuration to make\n" +
+             "sure that #{filename} exists and that jasminerice_reporter is included."
+      end
+
       passed = page.evaluate_script("window.jasmineRiceReporter.results.passedCount")
       failed = page.evaluate_script("window.jasmineRiceReporter.results.failedCount")
       total = page.evaluate_script("window.jasmineRiceReporter.results.totalCount")
       failures = page.evaluate_script("window.jasmineRiceReporter.failedSpecs")
       puts "Jasmine results - Passed: #{passed} Failed: #{failed} Total: #{total}"
+
       if failures.size > 0
         puts 'Jasmine failures:  '
         for suiteName,suiteFailures in failures
@@ -41,8 +59,6 @@ module Jasminerice
     def wait_for_finished
       reporter = page.evaluate_script("window.jasmineRiceReporter")
       if reporter.nil?
-        puts "jasmineRiceReporter not defined! Check your configuration to make\n" +
-             "sure that jasminerice_reporter is included in spec.js"
         raise "Reporter not found"
       end
 
--- lib/tasks/jasminerice-runner.rake
+++ lib/tasks/jasminerice-runner.rake
@@ -1,9 +1,10 @@
 namespace :jasminerice do
-  
+
   desc "run jasmine specs in jasmine rice"
-  task :run => :environment do
+  task :run, [:jasmine_environment] => :environment do |t, args|
     require "capybara/rails"
     require "jasminerice-runner/runner"
-    Jasminerice::Runner.new.run
+    args.with_defaults(jasmine_environment: nil)
+    Jasminerice::Runner.new.run(args[:jasmine_environment])
   end
 end
