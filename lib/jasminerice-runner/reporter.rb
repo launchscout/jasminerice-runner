01f71136aa404c5c8081eace5cb07a970f035b50
Sergey Kuchmistov <sergey.kuchmistov@gmail.com>
2013-07-05 22:49:14 +0300

    add simple colors to reports

--- lib/jasminerice-runner/reporter.rb
+++ lib/jasminerice-runner/reporter.rb
@@ -1,19 +1,20 @@
 module Jasminerice
   class Reporter
+    attr_reader :results
 
     def report(results)
-      puts "Jasmine results - Passed: #{results[:passed]} Failed: #{results[:failed]} Total: #{results[:total]}"
-      failures = results[:failures]
+      @results = results
+      total_report
 
       if failures.size == 0
         puts "Jasmine specs passed, yay!"
       else
-        report_failures(failures)
+        report_failures
         raise "Jasmine specs failed"
       end
     end
 
-    def report_failures(failures)
+    def report_failures
       puts 'Jasmine failures:  '
       for suiteName,suiteFailures in failures
         puts "  " + suiteName + "\n"
@@ -26,5 +27,25 @@ module Jasminerice
         puts "\n"
       end
     end
+
+    def total_report
+      text =  "Jasmine results - Passed: #{results[:passed]} Failed: #{results[:failed]} Total: #{results[:total]}"
+      if failures.any?
+        puts red text
+      else
+        puts green text
+      end
+    end
+
+    protected
+
+    def failures
+      @results[:failures]
+    end
+
+    def colorize(text, color_code) "#{color_code}#{text}\e[0m" ; end
+
+    def red(text); colorize(text, "\e[1m\e[31m"); end
+    def green(text); colorize(text, "\e[1m\e[32m"); end
   end
 end
